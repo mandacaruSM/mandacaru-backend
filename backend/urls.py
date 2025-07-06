d360be3c3c03159dcc3ede64a02853973e4a2b30
mandacaruSM <wgm.mandacaru@gmail.com>
2025-07-06 18:00:42 -0300

    Corrige rota de manutencao para manutencoes no urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/clientes/', include('apps.clientes.urls')),
     path('api/ordens/', include('apps.ordens_servico.urls')),
-    path('api/manutencao/', include('apps.manutencao.urls')),
+    path('api/manutencoes/', include('apps.manutencao.urls')),
     path('api/relatorios/', include('apps.relatorios.urls')),
     path('api/empreendimentos/', include('apps.empreendimentos.urls')),
     path('api/almoxarifado/', include('apps.almoxarifado.urls')),
