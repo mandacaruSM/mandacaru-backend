397eefc4214ec9a349765068f93782f37f65a893
mandacaruSM <wgm.mandacaru@gmail.com>
2025-07-07 07:30:09 -0300

    Adiciona rota de equipamentos na API

--- apps/equipamentos/urls.py
+++ apps/equipamentos/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import EquipamentoViewSet
 
 router = DefaultRouter()
 router.register(r'', EquipamentoViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+]
