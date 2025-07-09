de5e62e8e8079824ad6baae265b5f5be406a738a
mandacaruSM <wgm.mandacaru@gmail.com>
2025-07-09 17:34:56 -0300

    Atualiza ERP Mandacaru com ajustes recentes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,18 @@
+// next.config.js
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_API_URL: "https://mandacaru-backend-i2ci.onrender.com",
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://mandacaru-backend-i2ci.onrender.com/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
